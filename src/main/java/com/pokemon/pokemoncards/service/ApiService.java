e5155514a39b0c2bb195cd1cef904718c351f9b5
Hodon534 <hodon534@gmail.com>
2023-04-19 13:06:06 +0200

    requesting data from pokemontcg

---
+++ src/main/java/com/pokemon/pokemoncards/service/ApiService.java
@@ -0,0 +1,28 @@
+package com.pokemon.pokemoncards.service;
+
+import com.pokemon.pokemoncards.model.api.ApiLinkCreator;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.reactive.function.client.WebClient;
+
+//todo Number of cards = 16109, 3221 pages - 5 pageSize
+public class ApiService {
+    /**
+     * GET JSON from "pokemontcg.io" (Pokemon TCG API)
+     * request is for 5 random cards
+     * @return String
+     */
+    static ApiLinkCreator apiLinkCreator = new ApiLinkCreator();
+
+    public static String requestData(){
+        String url = apiLinkCreator.createLink();
+        System.out.println(url);
+        WebClient.Builder builder = WebClient.builder();
+
+        return builder.build()
+                .get()
+                .uri(url)
+                .retrieve()
+                .bodyToMono(String.class)
+                .block();
+    }
+}

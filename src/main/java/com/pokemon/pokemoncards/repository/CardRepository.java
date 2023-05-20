e63346476b61245fa110563f3e70effcaef80610
Hodon534 <hodon534@gmail.com>
2023-05-20 19:37:37 +0200

    Documentation added

--- src/main/java/com/pokemon/pokemoncards/repository/CardRepository.java
+++ src/main/java/com/pokemon/pokemoncards/repository/CardRepository.java
@@ -9,13 +9,30 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 import java.util.Optional;
 
+/**
+ * Repository of Pokemon Cards, necessary to access Database
+ */
 @Repository
 public interface CardRepository extends JpaRepository<CardEntity, String> {
 
+    /**
+     * Delete single pokemon card
+     * @param id - unique pokemon card ID
+     */
     void deleteCardById(String id);
 
+    /**
+     * Find single pokemon card
+     * @param id - unique pokemon card ID
+     * @return Pokemon Card value from DB, if any
+     */
     Optional<CardEntity> findCardById(String id);
 
+    /**
+     * Find multiple pokemon cards
+     * @param name - pokemon name
+     * @return List of Pokemon Card values from DB, if any
+     */
     Optional<List<CardEntity>> findCardByNameContaining(String name);
 
 }
